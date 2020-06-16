2456eb386e0442d839e15a89049edfcccd780a06
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-16 18:19:23 +0000

    Add character counter files.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,15 @@
+
+
 $(document).ready(function() {
   console.log("Ready is ready.")
-}
+
+
+  $("#tweet-text").keypress(function(){
+    console.log("Tweet-text is happenin'")
+  });
+  
+})
+
+
+//$(document).on('input', 'input:text', function() {});  WILL THIS WORK?
+
