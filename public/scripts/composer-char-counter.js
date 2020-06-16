bdf2d615208f76e091807833078635e8c99b1950
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-16 19:14:42 +0000

    Correctly implemented the counter functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,17 @@
 
 
 $(document).ready(function() {
-  console.log("Ready is ready.")
-
-
-  $("#tweet-text").keypress(function(){
+  $("#tweet-text").keyup(function(){
     console.log("Tweet-text is happenin'")
+    let countdown = 140 - this.value.length;
+    if (countdown < 0) {
+      $(this).parent().find(".counter").addClass("negative");
+    }
+    $(this)
+      .parent()
+      .find(".counter")
+      .val(countdown)
+
   });
   
 })
