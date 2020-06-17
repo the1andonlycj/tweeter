a3211d0331e143d766e8050ef1e523e875f1170e
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-17 00:04:27 +0000

    Adding styling for new tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,15 @@
 
 $(document).ready(function() {
   $("#tweet-text").keyup(function(){
-    console.log("Tweet-text is happenin'")
     let countdown = 140 - this.value.length;
     if (countdown < 0) {
       $(this).parent().find(".counter").addClass("negative");
+    } else {
+      $(this).parent().find(".counter").removeClass("negative");
     }
     $(this)
       .parent()
       .find(".counter")
       .val(countdown)
   });
-  
 })
