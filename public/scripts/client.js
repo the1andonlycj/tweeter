2119364b9b74db343e0bf8186f853660fe66e2bb
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-17 19:55:43 +0000

    Adjusted functionality for posting new tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,70 +1,78 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-$( document ).ready(function() {
-  console.log( "Client.js is ready!" );
-});
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+//Testing tweets:
+const tweetData1 = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-//Function for creating the new tweet elements:
+//Function for creating the new tweet elements array:
 const createTweetElement = function (tweetData) {
+  // let tweetsArr = [];
+  // for(tweet of data) {
+    let $tweet = `
+    <article class="posted-tweet">
+    <div class="tweet-header">
+    <div class="profile-pic">
+    <img src= "${tweetData.user.avatars}"}>
+    </div>
+    <div class="name">
+    <p>${tweetData.user.name}</p>
+    </div>
+    <div class="username">
+    <p>${tweetData.user.handle}</p>
+    </div>
+    </div>
+    <div class ="tweet-body">
+    <p> ${tweetData.content.text}</p>
+    </div>
+    <hr>
+    <div class="tweet-footer">
+    <div class="timestamp">
+    <p>${tweetData.created_at}</p>
+    </div>
+    <div class="icons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heartbeat"></i>
+    </div>
+    </div>
+    </article>
+    `;
+    console.log($tweet);
+    return $tweet;
+    // tweetsArr.push($tweet);
+  }
   
-  let $tweet = `
-  <article>
-  <div class="tweet-header">
-  <div class="profile-pic">
-  <p>${tweetData.user.avatars}</p>
-  </div>
-  <div class="name">
-  <p>${tweetData.user.name}</p>
-  </div>
-  <div class="username">
-  <p>${tweetData.user.handle}</p>
-  </div>
-  </div>
-  <div class ="tweet-body">
-  <p> ${tweetData.content.text}</p>
-  </div>
-  <hr>
-  <div class="tweet-footer">
-  <div class="timestamp">
-  <p>${tweetData.created_at}</p>
-  </div>
-  <div class="icons">
-  <i class="fas fa-flag"></i>
-  <i class="fas fa-retweet"></i>
-  <i class="fas fa-heartbeat"></i>
-  </div>
-  </div>
-  </article>
-  `;
+  //Function for sending the tweets to the page:
+  const renderTweets = function(data) {
+    for(tweet of data) {
+      $("#tweet-container").append(createTweetElement(tweet))
+    }
+  }
   
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-} 
-
-const $tweet = createTweetElement(tweetData);
-// const renderTweets = function(tweets) {
-  // THIS IS FOR LATER
-  // }
+  //Jquery declaration:
+  $(document).ready(function () {
+    console.log("Client.js is ready!");
+    renderTweets(tweetData1)
+  });
